0dbe6517a2343f8aab327cdf8d7f298888c2231f
Kunal Sharma <mailkunalsharma99@gmail.com>
2020-06-09 00:41:01 +0530

    Add files via upload

---
+++ LAN troubleshooter_V1.0.bat
@@ -0,0 +1,14 @@
+@echo off
+title Lan Troubleshooter V1.0
+echo By Kunal Sharma
+echo Make sure to run this in administrator mode.
+echo Executing commands for you
+echo Because you are too lazy to write them again and again.
+ipconfig/flushdns
+ipconfig/registerdns
+ipconfig/release
+ipconfig/renew
+echo I hope this solved your problem.
+echo Thank you for using LAN Troubleshooter V1.0
+echo By Kunal Sharma
+pause
